2baa22479cf3077ffa8b6ab74e8c68418bfdb92f
Muhammad Ramadhan <mramadhan742@gmail.com>
2021-11-20 12:36:05 +0700

    Added New Script
    Re - edit camera follow
    Add Lightpoint player
    Create boundaries screen

---
+++ Agni/Assets/Script/Enemy/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : SpawnEnemy
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CubeSpawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
