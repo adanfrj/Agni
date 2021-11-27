b38a9743c2c398aad425dd83873e3718f622237f
Muhammad Ramadhan <mramadhan742@gmail.com>
2021-11-27 15:44:21 +0700

    Add new file

---
+++ Agni/Assets/Script/Enemy/Zombie.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+    private Transform target; //the enemy's target
+    public float moveSpeed = 5; //move speed
+    public float distances;
+    
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, target.position) >= distances)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+        }
+
+        if (transform.position.x < target.position.x)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3 (1, 1, 1);
+        }
+
+
+
+
+
+        
+    }
+}
