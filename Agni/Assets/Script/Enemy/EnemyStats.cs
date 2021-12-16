33e8fbb0ced345f12980891c8ddb8a8bda7adb45
Muhammad Ramadhan <mramadhan742@gmail.com>
2021-12-16 18:58:52 +0700

    Add new file

---
+++ Agni/Assets/Script/Enemy/EnemyStats.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+    public int maxHealthEnemy = 3;
+    public int currentHealthEnemy;
+
+    public EnemyHealthBar healthBarEnemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealthEnemy = maxHealthEnemy;
+        healthBarEnemy.SetEnemyMaxHealth(maxHealthEnemy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TakeDamagePlayer(int damage)
+    {
+        currentHealthEnemy -= damage;
+        healthBarEnemy.SetEnemyHealth(currentHealthEnemy);
+    }
+
+    void OnCollisionEnter2D (Collision2D player)
+    {
+       if (player.transform.CompareTag("Player"))
+       {
+           TakeDamagePlayer(1);
+       }
+    }
+}
---
+++ Agni/Assets/Script/Player/PlayerStats.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int maxHealth = 3;
+    public int currentHealth;
+    public HealthBar healthBar;
+
+    private Vector3 respawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+        respawnPoint = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            transform.position = respawnPoint;
+            currentHealth = maxHealth;
+        }
+
+      
+    }
+
+    void OnCollisionEnter2D (Collision2D enemy)
+    {
+       if (enemy.transform.CompareTag("Enemy"))
+       {
+           TakeDamage(1);
+       }
+    }
+}
